8e5d4263fa4cef71ec0209d4522eb6173a871071
Shang-Yang Chen <sychen0584@gmail.com>
2019-02-11 16:51:07 -0600

    security updates

--- magnet/settings.py
+++ magnet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = bool(os.environ['DJANGO_DEBUG'])
+DEBUG = False
 
 ALLOWED_HOSTS = ["magnet-winterlab.herokuapp.com"]
 #ALLOWED_HOSTS = []
