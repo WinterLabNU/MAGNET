ec523c7ca244c9bfe92d26ea60b55a90c65cf271
Shang-Yang Chen <sychen0584@gmail.com>
2019-02-13 17:37:35 -0600

    Reformat code

--- magnet_v030/forms.py
+++ magnet_v030/forms.py
@@ -1,33 +1,55 @@
 from django import forms
 from .models import Dataset
 
+
 class UserForm(forms.Form):
-    
-    one_or_multiple = forms.ChoiceField(label='',choices=[('One','One'),("Multiple",'Multiple')],
-                                        widget=forms.RadioSelect,required=False)
-    
-    user_genes = forms.CharField(label='Gene List:',required = False, widget=forms.Textarea(attrs={'placeholder': 'Enter your list of query genes here','rows':4,'cols':15}))
-    user_genes_upload = forms.FileField(label="", required = False)
-    
-    user_background = forms.CharField(label='Background:',required = False, widget=forms.Textarea(attrs={'placeholder': 'Enter your list of background genes here','rows':4,'cols':15}))
-    ##
-    background_calc = forms.ChoiceField(label='',choices=[('Intersect','Intersect'),("User",'User')],
+
+    # radio button to ask whether the user is submitting one or multiple query gene lists
+    one_or_multiple = forms.ChoiceField(label='',
+                                        choices=[('One', 'One'),
+                                                 ('Multiple', 'Multiple')],
                                         widget=forms.RadioSelect, required=False)
-    
-    user_background_upload = forms.FileField(label="", required = False)
-    
-    user_selected_datasets = forms.ModelChoiceField(queryset= Dataset.objects.all(),empty_label=None,label='Include the following datasets from the database :',required=False,widget=forms.HiddenInput())
-    
-    def __init__(self,*args,**kwargs):
-        super(UserForm,self).__init__(*args,**kwargs)
-        self.fields['user_selected_datasets'].choices = [(e.id,e.dataset_name) for e in Dataset.objects.all()]
 
+    # text area for pasting a single query gene list
+    user_genes = forms.CharField(label='Gene List:',
+                                 required=False,
+                                 widget=forms.Textarea(attrs={'placeholder': 'Enter your list of query genes here',
+                                                              'rows': 4, 'cols': 15}))
 
-    #def clean(self):
-        #cleaned_data = super(UserForm,self).clean()
-        #user_genes = cleaned_data.get('user_genes')
-        #user_background = cleaned_data.get('user_background')
-        #user_selected_datasets = cleaned_data.get('user_selected_datasets')
+    # file upload for query gene lists
+    user_genes_upload = forms.FileField(label="", required=False)
 
-        #if not user_genes and not user_background and not user_selected_datasets:
-            #raise forms.ValidationError('Please fill all the input values for enrichment calculation!')
+    # text area for pasting a background gene list
+    user_background = forms.CharField(label='Background:',
+                                      required=False,
+                                      widget=forms.Textarea(attrs={'placeholder': 'Enter your list of background genes here',
+                                                                   'rows': 4,
+                                                                   'cols': 15}))
+
+    # file upload for the background gene list
+    user_background_upload = forms.FileField(label="", required=False)
+
+    # radio button for background processing modes
+    background_calc = forms.ChoiceField(label='',
+                                        choices=[('Intersect', 'Intersect'), ("User", 'User')],
+                                        widget=forms.RadioSelect, required=False)
+
+    # checkboxes for datasets to include
+    user_selected_datasets = forms.ModelChoiceField(queryset=Dataset.objects.all(),
+                                                    empty_label=None,
+                                                    label='Include the following datasets from the database :',
+                                                    required=False,
+                                                    widget=forms.HiddenInput())
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['user_selected_datasets'].choices = [(dataset.id, dataset.dataset_name) for dataset in Dataset.objects.all()]
+
+    # def clean(self):
+        # cleaned_data = super(UserForm,self).clean()
+        # user_genes = cleaned_data.get('user_genes')
+        # user_background = cleaned_data.get('user_background')
+        # user_selected_datasets = cleaned_data.get('user_selected_datasets')
+
+        # if not user_genes and not user_background and not user_selected_datasets:
+            # raise forms.ValidationError('Please fill all the input values for enrichment calculation!')
--- magnet_v030/models.py
+++ magnet_v030/models.py
@@ -2,45 +2,65 @@ from django.db import models
 
 # Create your models here.
 
+
 class Gene(models.Model):
+
+    # stores main gene symbols
     gene_symbol = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.gene_symbol
 
+
 class Alias(models.Model):
+
+    # stores known gene symbol aliases and ensembl ids of Gene object
     gene = models.ForeignKey(Gene, on_delete=models.CASCADE)
     alias_name = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.gene.gene_symbol + "-" + self.alias_name
-    
+
+
 class Dataset(models.Model):
-    dataset_name = models.CharField(max_length=200, unique = True)
+
+    # stores meta info of the datasets, associates with clusters
+    # all information except dataset_name are entered in admin interface
+    dataset_name = models.CharField(max_length=200, unique=True)
     full_title = models.TextField(blank=True)
     authors = models.TextField(blank=True)
-    publication_date = models.DateField(blank=True,null=True)
-    journal = models.CharField(max_length=100,blank=True)
+    publication_date = models.DateField(blank=True, null=True)
+    journal = models.CharField(max_length=100, blank=True)
     link_to_pubmed = models.URLField(blank=True)
     abstract = models.TextField(blank=True)
-    
+
     def __str__(self):
         return self.dataset_name
 
+
 class Cluster(models.Model):
+
+    # child table of Dataset
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
     cluster_number = models.IntegerField()
     cluster_description = models.CharField(max_length=500, blank=True)
-    
-    class Meta: 
-        ordering=["cluster_number"]
-    
+
+    class Meta:
+        ordering = ["cluster_number"]
+
     def __str__(self):
         return self.cluster_description + " (" + str(self.cluster_number) + ")"
-    
+
+
 class Annotation(models.Model):
+
+    # links a Cluster to a Gene
     gene = models.ForeignKey(Gene, on_delete=models.CASCADE)
     cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
-    
+
     def __str__(self):
-        return str(self.gene) +" --- "+ str(self.cluster.dataset) +" --- "+ str(self.cluster)
+        return '{} --- {} --- {}'.format(
+            str(self.gene),
+            str(self.cluster.dataset),
+            str(self.cluster)
+        )
